304d57de744365eb38ff112e879911f1f90ddcc2
ben <bjelinek42@gmail.com>
2022-03-18 09:26:35 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/balls" => "balls#index"
+  get "/balls/:id" => "balls#show"
+  post "/balls" => "balls#create"
+  patch "/balls/:id" => "balls#update"
+  delete "/balls/:id" => "balls#destroy"
 end
