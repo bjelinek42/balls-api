e5b77746dbb6f6926814ab1112fe79de8215d7fa
ben <bjelinek42@gmail.com>
2022-03-18 09:33:30 -0600

    seeds and index/show controller

--- app/controllers/balls_controller.rb
+++ app/controllers/balls_controller.rb
@@ -1,2 +1,22 @@
 class BallsController < ApplicationController
+
+  def index
+    ball = Ball.all
+    render json: ball
+  end
+
+  def show
+    ball = Ball.find_by(id: params[:id])
+    render json: ball
+  end
+
+  def create
+    
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ball = Ball.new({name: "Basketball", size: "M", price: 20})
+ball.save
+
+ball = Ball.new({name: "Baseball", size: "S", price: 5})
+ball.save
+
+ball = Ball.new({name: "Football", size: "S", price: 12})
+ball.save
+
+ball = Ball.new({name: "Tennis Ball", size: "S", price: 3})
+ball.save
+
+ball = Ball.new({name: "Medicine Ball", size: "L", price: 35})
+ball.save
