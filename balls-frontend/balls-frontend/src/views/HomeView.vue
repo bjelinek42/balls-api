144a35140e055a83895f8b633cb0cceec487161c
ben <bjelinek42@gmail.com>
2022-04-05 16:57:00 -0600

    start frontend

---
+++ balls-frontend/balls-frontend/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      balls: []
+    };
+  },
+  created: function () {
+    console.log(this.balls)
+  },
+  methods: {
+    ballsIndex: function () {
+      axios.get("http://localhost:3000/balls").then(request =>
+        console.log(request.data))
+    }
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
